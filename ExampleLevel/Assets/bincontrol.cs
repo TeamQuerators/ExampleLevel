5ba1c67d4887f5565dbf2cff1edf40b717157fbb
Team Querators <teamquerators@gmail.com>
2019-03-28 11:37:38 +0800

    Third Commit
    - Start coding
    - Include toppings graphics

---
+++ ExampleLevel/Assets/bincontrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bincontrol : MonoBehaviour
+{
+    public Transform pizzaObj;
+    public Transform pastaObj;
+    public Transform marinarasauceObj;
+    public Transform cheeseObj;
+    public Transform plateObj;
+    public Transform trayObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        if (gameObject.name == "trayBin")
+        {
+            if (gameplay.servingBoardS1 == "empty")
+            {
+                Instantiate(trayObj, new Vector2(-1, -1f), trayObj.rotation);
+                Instantiate(pastaObj, new Vector2(-1, -1.25f), pastaObj.rotation);
+                gameplay.servingBoardS1 = "justPasta";
+                gameplay.selectedSlot = 1;
+            }
+
+        }
+    }
+}
+         
---
+++ ExampleLevel/Assets/gameplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameplay : MonoBehaviour
+{
+    public static string servingBoardS1;
+    public static int selectedSlot = 0;
+    public static int selectedPasta = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
