6de1f7ca984eb3aabdc490c505bf0380b26fa8be
Team Querators <teamquerators@gmail.com>
2019-04-04 11:58:37 +0800

    Fifth commit
    Added graphics
    Added working scripts

--- ExampleLevel/Assets/bincontrol.cs
+++ ExampleLevel/Assets/bincontrol.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class bincontrol : MonoBehaviour
 {
+    public Transform platetopObj;
+    public Transform platebottomObj;
     public Transform pizzaObj;
     public Transform pastaObj;
     public Transform marinarasauceObj;
     public Transform cheeseObj;
-    public Transform plateObj;
     public Transform trayObj;
+
+    /* Instantiate variables
+     * justplate = plate  
+     * pastaplate = plate + pasta 
+     * pastasauce = plate + pasta + sauce 
+     * pasta cheese = plate+ pasta + sauce + cheese
+     */
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +34,92 @@ public class bincontrol : MonoBehaviour
 
     void OnMouseDown()
     {
+        //INSTANTIATE VARIABLES
+        
+        //PLATE
+        
         if (gameObject.name == "platestackBin")
         {
             if (gameplay.servingBoardS1 == "empty")
             {
-                Instantiate(plateObj, new Vector2(5, -2f), plateObj.rotation);
-                gameplay.servingBoardS1 = "justPlate";
-                gameplay.selectedSlot = 1;
+                Instantiate(platetopObj, new Vector2(2.75f, -0.75f), platetopObj.rotation);
+                Instantiate(platebottomObj, new Vector2(2.75f, -0.75f), platebottomObj.rotation);
+                gameplay.servingBoardS1 = "justplate";
+                //gameplay.selectedSlot = 1;
             }
-
+            else 
+                if (gameplay.servingBoardS2 == "empty")
+                {
+                    Instantiate(platetopObj, new Vector2(2.75f, -1.75f), platetopObj.rotation);
+                    Instantiate(platebottomObj, new Vector2(2.75f, -1.75f), platebottomObj.rotation);
+                    gameplay.servingBoardS2 = "justplate";
+                    //gameplay.selectedSlot = 1;
+                }
         }
+       
+
+
+        //PASTA
+        if (gameObject.name == "spagstrainerBin")
+        {
+            if (gameplay.servingBoardS1 == "justplate")
+            {
+                Instantiate(pastaObj, new Vector2(2.75f, -1.15f), pastaObj.rotation);
+                //Instantiate(pastaObj, new Vector2(2.75f, -1.15f), pastaObj.rotation);
+                gameplay.servingBoardS1 = "pastaplate";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "justplate")
+            {
+                Instantiate(pastaObj, new Vector2(2.75f, -2.15f), pastaObj.rotation);
+                //Instantiate(pastaObj, new Vector2(2.75f, -1.15f), pastaObj.rotation);
+                gameplay.servingBoardS2 = "pastaplate";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+
+        //Marinara Sauce
+        
+        if (gameObject.name == "sauceBin")
+        {
+            if (gameplay.servingBoardS1 == "pastaplate")
+            {
+                Instantiate(marinarasauceObj, new Vector2(2.75f, -0.81f), marinarasauceObj.rotation);
+                gameplay.servingBoardS1 = "pastasauce";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "pastaplate")
+            {
+                Instantiate(marinarasauceObj, new Vector2(2.75f, -1.81f), marinarasauceObj.rotation);
+                gameplay.servingBoardS2 = "pastasauce";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+
+        //Cheese
+        if (gameObject.name == "cheesebowlBin")
+        {
+            if (gameplay.servingBoardS1 == "pastasauce")
+            {
+                Instantiate(cheeseObj, new Vector2(2.75f, -0.8f), cheeseObj.rotation);
+                Instantiate(cheeseObj, new Vector2(2.75f, -0.8f), cheeseObj.rotation);
+                gameplay.servingBoardS1 = "pastacheese";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "pastasauce")
+            {
+                Instantiate(cheeseObj, new Vector2(2.75f, -1.8f), cheeseObj.rotation);
+                Instantiate(cheeseObj, new Vector2(2.75f, -0.8f), cheeseObj.rotation);
+                gameplay.servingBoardS2 = "pastacheese";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+
+
     }
+
 }
          
--- ExampleLevel/Assets/gameplay.cs
+++ ExampleLevel/Assets/gameplay.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class gameplay : MonoBehaviour
 {
-    public static string servingBoardS1;
+    public static string servingBoardS1 = "empty";
+    public static string servingBoardS2 = "empty";
     public static int selectedSlot = 0;
     public static int selectedPasta = 0;
 
+    
 
     // Start is called before the first frame update
     void Start()
