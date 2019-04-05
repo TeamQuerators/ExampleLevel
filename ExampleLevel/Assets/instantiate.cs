c81d4e9db47e998c85bc2ad548b352c817307f35
Team Querators <teamquerators@gmail.com>
2019-04-05 11:39:19 +0800

    Sixth commit
    Test resize
    Instantiate object

---
+++ ExampleLevel/Assets/instantiate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiate : MonoBehaviour
+{
+    public GameObject plate;
+    public Transform plateObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject newplate = Instantiate(plate, transform.position, transform.rotation) as GameObject;
+        //newplate.transform.SetParent(GameObject.FindGameObjectWithTag("plate").transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "platestackBin")
+        {
+            if (gameplay.servingBoardS1 == "empty")
+            {
+                GameObject newplate = Instantiate(plate, new Vector2(0,0) , transform.rotation) as GameObject;
+                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("plate").transform, false);
+
+                gameplay.servingBoardS1 = "justplate";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "empty")
+            {
+                GameObject newplate = Instantiate(plate, new Vector2(0, -1), transform.rotation) as GameObject;
+                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("plate").transform, false);
+
+                gameplay.servingBoardS2 = "justplate";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+    }
+}
