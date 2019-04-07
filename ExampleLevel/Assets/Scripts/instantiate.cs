4299987fa1d4f17601c44a22a1ccf2fb097778f2
Team Querators <teamquerators@gmail.com>
2019-04-08 00:54:06 +0800

    First commit
    - Test instantiate obj in diff scene
    - Worked but not in main scene

--- ExampleLevel/Assets/Scripts/instantiate.cs
+++ ExampleLevel/Assets/Scripts/instantiate.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class instantiate : MonoBehaviour
 {
-    public GameObject plate;
+    //public GameObject plate;
     public Transform plateObj;
+    public Transform pastaObj;
+    public Transform marinarasauceObj;
+    public Transform cheeseObj;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +28,49 @@ public class instantiate : MonoBehaviour
         {
             if (gameplay.servingBoardS1 == "empty")
             {
+                Instantiate(plateObj, new Vector2(0, 0), plateObj.rotation);
+                
+                gameplay.servingBoardS1 = "justplate";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "empty")
+            {
+                Instantiate(plateObj, new Vector2(2, -2f), plateObj.rotation);
+                
+                gameplay.servingBoardS2 = "justplate";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+
+        //PASTA
+        if (gameObject.name == "spagstrainerBin")
+        {
+            if (gameplay.servingBoardS1 == "justplate")
+            {
+                Instantiate(pastaObj, new Vector2(0, 0), pastaObj.rotation);
+                //Instantiate(pastaObj, new Vector2(2.75f, -1.15f), pastaObj.rotation);
+                gameplay.servingBoardS1 = "pastaplate";
+                //gameplay.selectedSlot = 1;
+            }
+            else
+                if (gameplay.servingBoardS2 == "justplate")
+            {
+                Instantiate(pastaObj, new Vector2(2.75f, -2.15f), pastaObj.rotation);
+                //Instantiate(pastaObj, new Vector2(2.75f, -1.15f), pastaObj.rotation);
+                gameplay.servingBoardS2 = "pastaplate";
+                //gameplay.selectedSlot = 1;
+            }
+        }
+
+
+
+        /* if (gameObject.name == "platestackBin")
+        {
+            if (gameplay.servingBoardS1 == "empty")
+            {
                 GameObject newplate = Instantiate(plate, new Vector2(0,0) , transform.rotation) as GameObject;
-                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("plate").transform, false);
+                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
 
                 gameplay.servingBoardS1 = "justplate";
                 //gameplay.selectedSlot = 1;
@@ -35,11 +79,11 @@ public class instantiate : MonoBehaviour
                 if (gameplay.servingBoardS2 == "empty")
             {
                 GameObject newplate = Instantiate(plate, new Vector2(0, -1), transform.rotation) as GameObject;
-                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("plate").transform, false);
+                newplate.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
 
                 gameplay.servingBoardS2 = "justplate";
                 //gameplay.selectedSlot = 1;
             }
-        }
+        } */
     }
 }
